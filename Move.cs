ef0d7fe9a0a2b25cd2d9675990f6669d8619b876
Christopher Pearl <53843747+yellavestudios@users.noreply.github.com>
2024-10-03 14:07:48 -0700

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 2f;
+    void Start() {
+
+        //direction = goal.transform.position - transform.position;
+       
+        //this.transform.Translate(direction);
+    }
+
+    private void LateUpdate() {
+
+        direction = goal.transform.position - transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if (direction.magnitude > 1)
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
+    }
+}
